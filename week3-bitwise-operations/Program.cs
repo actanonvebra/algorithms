3c5015366824cf6be38bbba6c73eebc5edf1a1db
actanonvebra <ibrahimserhatbulut@gmail.com>
2024-03-12 11:42:35 +0300

    Week 3 created, 2 samples added.

---
+++ week3-bitwise-operations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region example-1
+
+        uint a = 1;  // 0000 0000 0000 0000 0000 0000 0000 0001
+
+        a = a | 0x3; // 0000 0000 0000 0000 0000 0000 0000 0011
+
+        Console.WriteLine("a or 0x3 : "+a);
+
+        #endregion
+
+        #region example-2 (odd or even)
+
+        /*
+        If the first bit of the number is 1, the number is even.
+        If the first bit of the number is 0, the number is odd.
+        */
+
+
+
+        #endregion
+
+        Console.ReadLine();
+    }
+}
