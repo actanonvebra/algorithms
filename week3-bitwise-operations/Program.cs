eca2bfbb3910117c9c14c3360e87f5edcdb2840b
actanonvebra <ibrahimserhatbulut@gmail.com>
2024-03-14 13:50:12 +0300

    example 3 and example 4 added

--- week3-bitwise-operations/Program.cs
+++ week3-bitwise-operations/Program.cs
@@ -4,17 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        #region example-1
+        #region [example-1] & (and operator)
 
-        uint a = 1;  // 0000 0000 0000 0000 0000 0000 0000 0001
+        // uint a = 1;  // 0000 0000 0000 0000 0000 0000 0000 0001
 
-        a = a | 0x3; // 0000 0000 0000 0000 0000 0000 0000 0011
+        // a = a | 0x3; // 0000 0000 0000 0000 0000 0000 0000 0011
 
-        Console.WriteLine("a or 0x3 : "+a);
+        // Console.WriteLine("a or 0x3 : "+a);
 
         #endregion
 
-        #region example-2 (odd or even)
+        #region [example-2] (odd or even)
 
         /*
         If the first bit of the number is 1, the number is even.
@@ -36,12 +36,95 @@ class Program
         0000 0000 0000 0000 0000 0000 0000 0110 --> 6
         0000 0000 0000 0000 0000 0000 0000 1110 --> 14
         */
+        #endregion
 
-        
+        #region [example-3] (negative or positive, bit)
+
+        /* Number is a 32 bit integer type.
+        0000 0000 0000 0000 0000 0000 0000 0000 -->0 
+        0000 0000 0000 0000 0000 0000 0000 0001 -->1 
+
+        1000 0000 0000 0000 0000 0000 0000 0001 -->-1
+        |
+        |
+        ¯¯¯¯ highest level bit (1), result = - (negative)
+
+        */
 
 
         #endregion
 
+        #region [example-4] (print bits of a number)
+
+                /*
+        The following operations are as follows
+
+        number x
+        0000 0000 0000 0000 0000 0000 0000 0001 
+        
+        number mask
+        1000 0000 0000 0000 0000 0000 0000 0000
+
+        for loop
+
+        i=0,
+
+        if x&mask == 0
+
+        result, respectively for each bit value
+        x -->       0000 0000 0000 0000 0000 0000 0000 0001
+        mask -->    1000 0000 0000 0000 0000 0000 0000 0000
+        result -->  0000 0000 0000 0000 0000 0000 0000 0000
+
+        if block return true, Console.Write("0");
+
+        i=1,
+
+        if x&mask == 0
+        
+        result, respectively for each bit value
+        x -->       0000 0000 0000 0000 0000 0000 0000 0001
+        mask -->    0100 0000 0000 0000 0000 0000 0000 0000 (right shift (mask = mask>>1))
+        result -->  0000 0000 0000 0000 0000 0000 0000 0000
+
+        .
+        .
+        .
+        .
+        .
+        .
+
+        i=31 (32 in total)
+
+        else,
+
+        result, respectively for each bit value
+        x -->       0000 0000 0000 0000 0000 0000 0000 0001
+        mask -->    0100 0000 0000 0000 0000 0000 0000 0001
+        result -->  0000 0000 0000 0000 0000 0000 0000 0001
+
+        if block return false
+        else --> Console.Write("1");
+
+        */
+
+        int x = unchecked((int)0x00000001);
+        int mask = unchecked((int)0x80000000);
+        for(int i=0; i<32; i++){
+            if ((x & mask)==0){
+                Console.Write("0");
+            }
+            else{
+                Console.Write("1");
+            }
+
+            mask=mask>>1;
+        }
+
+
+        #endregion
+
+        
         Console.ReadLine();
     }
 }
