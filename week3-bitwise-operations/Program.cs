bdd2c3a8f250738cb20e0decc2634017efce4b4d
actanonvebra <ibrahimserhatbulut@gmail.com>
2024-03-15 13:20:38 +0300

    example6-7-8 added.

--- week3-bitwise-operations/Program.cs
+++ week3-bitwise-operations/Program.cs
@@ -126,34 +126,118 @@ class Program
 
         #region [example-5] (set bit 5 of the number to 1)
 
-        uint number=0x00000000; // 0000 0000 0000 0000 0000 0000 0000 0000
-                                // decimal notation : 0
-        // uint number = 0; 
-        // uint number = 0x0;
+        // uint number=0x00000000; // 0000 0000 0000 0000 0000 0000 0000 0000
+        //                         // decimal notation : 0
+        // // uint number = 0; 
+        // // uint number = 0x0;
 
-        uint mask=0x0000010;   // 0000 0000 0000 0000 0000 0000 0001 0000
-                                // decimal notation : 16
+        // uint mask=0x0000010;   // 0000 0000 0000 0000 0000 0000 0001 0000
+        //                         // decimal notation : 16
 
         
-        number=number|mask;    
-        // number ---> 0000 0000 0000 0000 0000 0000 0000 0000
-        // mask   ---> 0000 0000 0000 0000 0000 0000 0001 0000
-        //  | (or) --> 0000 0000 0000 0000 0000 0000 0001 0000
-        Console.WriteLine("New number value: "+number);
+        // number=number|mask;    
+        // // number ---> 0000 0000 0000 0000 0000 0000 0000 0000
+        // // mask   ---> 0000 0000 0000 0000 0000 0000 0001 0000
+        // //  | (or) --> 0000 0000 0000 0000 0000 0000 0001 0000
+        // Console.WriteLine("New number value: "+number);
 
 
         #endregion
 
         #region [example-5.1] (set bit 17 of the number to 1)
 
-        uint number1=0x00000000; // 0000 0000 0000 0000 0000 0000 0000 0000
-        Console.WriteLine("Number1 value: "+number1);
+        // uint number1=0x00000000; // 0000 0000 0000 0000 0000 0000 0000 0000
+        // Console.WriteLine("Number1 value: "+number1);
 
-        uint mask1=0x00010000;   // 0000 0000 0000 0001 0000 0000 0000 0000
-        Console.WriteLine("Mask1 value: "+mask1);
+        // uint mask1=0x00010000;   // 0000 0000 0000 0001 0000 0000 0000 0000
+        // Console.WriteLine("Mask1 value: "+mask1);
 
-        number1=number1|mask1;
-        Console.WriteLine("Number1 new value: "+number1);
+        // number1=number1|mask1;
+        // Console.WriteLine("Number1 new value: "+number1);
+
+        #endregion
+
+        #region [example-6] (shift commands)
+        
+        /*
+        Right Shift | number = number >> 1; 
+        thnink a 4-bit number...
+        1 1 0 0 
+        right shift >>1
+        0 1 1 0
+        right shift >>1
+        0 0 1 1 
+        right shift >>1
+        0 0 0 1
+        right shift >>1
+        0 0 0 0
+
+        Left shift | number = number << 1; 
+        
+        1 1 0 0
+        left shift <<1;
+        1 0 0 0
+        left shift <<1;
+        0 0 0 0
+               
+        */
+
+        // uint number3=0x00000008; // 0000 0000 0000 0000 0000 0000 0000 1000 | decimal value = 8
+        // Console.WriteLine("number 3, decimal value = "+number3);
+        // number3 = number3 >> 1;  // 0000 0000 0000 0000 0000 0000 0000 0100 | decimal value = 4
+        // Console.WriteLine("(1 time right shift) number 3, decimal value = "+number3);
+        // number3 = number3 >> 1;  // 0000 0000 0000 0000 0000 0000 0000 0010 | devimal value = 2
+        // Console.WriteLine("(1 time right shift) number 3, decimal value = "+number3);
+        // number3 = number3 >> 1;  // 0000 0000 0000 0000 0000 0000 0000 0001 | devimal value = 1
+        // Console.WriteLine("(1 time right shift) number 3, decimal value = "+number3);
+        // number3 = number3 >> 1;  // 0000 0000 0000 0000 0000 0000 0000 0000 | devimal value = 0
+        // Console.WriteLine("(1 time right shift) number 3, decimal value = "+number3);
+
+
+        // uint number4=0x20000000; // 0010 0000 0000 0000 0000 0000 0000 0000 | 2^29 
+        // Console.WriteLine("number 3, decimal value = "+number4);
+        // number4 = number4 << 1;  // 0100 0000 0000 0000 0000 0000 0000 0000 | 2^30
+        // Console.WriteLine("(1 time left shift) number 4, decimal value = "+number4);
+        // number4 = number4 << 1;  // 1000 0000 0000 0000 0000 0000 0000 0000 | 2^31 (max value)
+        // Console.WriteLine("(1 time left shift) number 4, decimal value = "+number4);
+        // number4 = number4 << 1;  // 0000 0000 0000 0000 0000 0000 0000 0000 | decimal value = 0 
+
+
+        
+        #endregion
+
+        #region [example-7] (How many bits are 1?)
+
+        // uint number=0xffbb0000;
+        // uint mask=1;
+        // int counter=0;
+        // for(int i=0; i<32; i++){
+        //     if((number&mask)==1){
+        //         counter++;
+        //     }
+        //     number=number>>1;
+        // }
+        // Console.WriteLine("Counter: "+counter);
+
+        #endregion
+
+        #region [example-8] (write binary)
+
+        // uint k = 0xffbb00a0; // 11111111101110110000000010100000
+        // uint mask = 0x80000000; // 100000000000000000000000000000
+        // for(int i=0; i<32; i++){
+        //     if((k&mask)!=0){
+        //         Console.Write("1");
+        //     }
+        //     else{
+        //         Console.Write("0");
+        //     }
+        //     mask =mask >>1;
+        // }
+
+        #endregion
+
+        #region [example-9] (1 bits --> 0)
 
         #endregion
 
--- week3-bitwise-operations/obj/Debug/net7.0/week3-bitwise-operations.AssemblyInfo.cs
+++ week3-bitwise-operations/obj/Debug/net7.0/week3-bitwise-operations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week3-bitwise-operations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca2bfbb3910117c9c14c3360e87f5edcdb2840b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6558b8d6d460b6220c0477025a154355c57ba4b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("week3-bitwise-operations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week3-bitwise-operations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
